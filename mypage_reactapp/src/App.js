afc37c048129cef95cb535a1d53785693726a433
Simona71830 <skombarova.simona@gmail.com>
2023-07-17 21:05:26 +0200

    added FE

---
+++ mypage_reactapp/src/App.js
@@ -0,0 +1,35 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { Home } from "./Home";
+import { User } from "./User";
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App container">
+        <h3 className="d-flex justify-content-center m-3">
+          React JS Frontend Simona
+        </h3>
+        <nav className="navbar navbar-expand-sm bg-light navbar-datk">
+          <u1 className="navbar-nav">
+            <li className="nav-item- m-1">
+              <Link className="btn btn-light btn-outline-primary" to="/home">
+                Home
+              </Link>
+              <Link className="btn btn-light btn-outline-primary" to="/user">
+                User
+              </Link>
+            </li>
+          </u1>
+        </nav>
+        <Routes>
+          <Route path="/home" component={Home} />
+          <Route path="/user" component={User} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;

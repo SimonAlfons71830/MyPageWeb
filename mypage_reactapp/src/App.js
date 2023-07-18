7cd43af6f3b580058ef0771abc71853bed79572b
Simona71830 <skombarova.simona@gmail.com>
2023-07-18 19:29:01 +0200

    created navbar and fixed width of App

--- mypage_reactapp/src/App.js
+++ mypage_reactapp/src/App.js
@@ -1,31 +1,23 @@
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
 import { Home } from "./Home";
 import { User } from "./User";
 import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+import Navbar from "./Navbar"; // Import the Navbar component
+import { Contact } from "./Contact";
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="App container">
+      <div className="App">
+        <Navbar /> {/* Include the Navbar component */}
         <h3 className="d-flex justify-content-center m-3">
           React JS Frontend Simona
         </h3>
-        <nav className="navbar navbar-expand-sm bg-light navbar-datk">
-          <u1 className="navbar-nav">
-            <li className="nav-item- m-1">
-              <Link className="btn btn-light btn-outline-primary" to="/home">
-                Home
-              </Link>
-              <Link className="btn btn-light btn-outline-primary" to="/user">
-                User
-              </Link>
-            </li>
-          </u1>
-        </nav>
         <Routes>
           <Route path="/home" Component={Home} />
           <Route path="/user" Component={User} />
+          <Route path="/contact" Component={Contact} />
         </Routes>
       </div>
     </BrowserRouter>
---
+++ mypage_reactapp/src/Contact.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export class Contact extends Component {
+  render() {
+    return (
+      <div>
+        <h3>This will be my contact page</h3>
+      </div>
+    );
+  }
+}
